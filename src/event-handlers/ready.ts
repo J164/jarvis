a172ce7723d6263f1324ffa26f52206d5b7678d4
J164 <43323130+J164@users.noreply.github.com>
2023-06-15 21:11:25 -0500

    add task scheduler

--- src/event-handlers/ready.ts
+++ src/event-handlers/ready.ts
@@ -1,9 +1,29 @@
+import { readdir } from 'node:fs/promises';
+import { type ScheduleOptions, schedule } from 'node-cron';
 import { globalLogger } from '../util/logger.js';
 
 const logger = globalLogger.child({
 	name: 'ready-event',
 });
 
-export function onReady(): void {
+type Task = {
+	cronExpression: string;
+	handler: () => void;
+	scheduleOptions?: ScheduleOptions;
+};
+
+export async function onReady(): Promise<void> {
 	logger.info('Login success!');
+
+	const tasks = await readdir('./tasks');
+
+	await Promise.all(
+		tasks.map(async (file) => {
+			const {
+				task: { cronExpression, handler, scheduleOptions },
+			} = (await import(`../tasks/${file}`)) as { task: Task };
+
+			schedule(cronExpression, handler, scheduleOptions);
+		}),
+	);
 }
