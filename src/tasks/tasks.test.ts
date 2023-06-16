d25e84c9c590aa9b12ce484a042aa3e7cab15fb8
J164 <43323130+J164@users.noreply.github.com>
2023-06-15 21:45:41 -0500

    fix test

--- src/tasks/tasks.test.ts
+++ src/tasks/tasks.test.ts
@@ -3,12 +3,16 @@ import { describe, expect, it } from 'vitest';
 import { validate } from 'node-cron';
 import { type Task } from '../util/schedule-tasks.js';
 
-const files = await readdir('./tasks');
+const files = await readdir('./src/tasks');
 
 const tasks = await Promise.all(
-	files.map(async (file) => {
-		return ((await import(`../tasks/${file}`)) as { task: Task }).task;
-	}),
+	files
+		.filter((file) => {
+			return !file.endsWith('test.ts');
+		})
+		.map(async (file) => {
+			return ((await import(`../tasks/${file}`)) as { task: Task }).task;
+		}),
 );
 
 describe.each(tasks)('well-formedness of task "$name"', ({ cronExpression }) => {
